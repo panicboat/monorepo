fe9d1ffc2dd4e3dfb5f076dd72c0315044c5f3c2
panicboat <panicboat@gmail.com>
2024-06-05 17:48:35 +0900

    patch (#20)

--- .github/actions/scaffold-argo-workflows/spec/base/service_account_spec.rb
+++ .github/actions/scaffold-argo-workflows/spec/base/service_account_spec.rb
@@ -3,13 +3,13 @@ require_relative '../../src/base/service_account'
 
 describe 'ServiceAccount' do
   let(:env) do
-    { workspace: '/app', service: 'service', owner: 'owner', namespace: 'namespace', kind: 'kind', name: 'name', service_account: 'service_account' }
+    { workspace: '/app', service: 'service', owner: 'owner', namespace: 'namespace', kind: 'kind', name: 'name' }
   end
   let(:_service_account) { Base::ServiceAccount.new(env[:service], env[:owner], env[:namespace], env[:kind], env[:name]) }
 
   describe '#_workflow' do
     it 'returns the correct ServiceAccount' do
-      result = _service_account.create(env[:service_account])
+      result = _service_account.create
       expect(result).to be_a(Hash)
       expect(result[:kind]).to eq('ServiceAccount')
     end
--- .github/actions/scaffold-argo-workflows/spec/base_spec.rb
+++ .github/actions/scaffold-argo-workflows/spec/base_spec.rb
@@ -3,9 +3,9 @@ require_relative '../src/base'
 
 describe 'Base' do
   let(:env) do
-    { workspace: '/app', service: 'service', owner: 'owner', namespace: 'namespace', kind: 'kind', name: 'name', service_account: 'service_account' }
+    { workspace: '/app', service: 'service', owner: 'owner', namespace: 'namespace', kind: 'kind', name: 'name' }
   end
-  let(:_base) { BaseManifest.new(env[:workspace], env[:service], env[:owner], env[:namespace], env[:kind], env[:name], env[:service_account]) }
+  let(:_base) { BaseManifest.new(env[:workspace], env[:service], env[:owner], env[:namespace], env[:kind], env[:name]) }
   let(:kustomization) do
     {
       apiVersion: 'kustomize.config.k8s.io/v1beta1',
@@ -24,23 +24,23 @@ describe 'Base' do
 
   describe '#_kustomization' do
     it 'returns the correct kustomization (create)' do
-      result = _base.send(:_kustomization, {}, true)
+      result = _base.send(:_kustomization, {}, true, true)
       expect(result).to be_a(Hash)
       expect(result[:configMapGenerator]).to be_a(Array)
       expect(result[:configMapGenerator]).to eq([{name: env[:name], options: {disableNameSuffixHash: true}}])
       expect(result[:resources]).to be_a(Array)
-      expect(result[:resources]).to eq(["serviceaccount/#{env[:service_account]}.yaml", "rolebinding/#{env[:name]}-#{env[:service_account]}.yaml", "#{env[:kind]}/#{env[:name]}.yaml"])
+      expect(result[:resources]).to eq(["serviceaccount/#{env[:name]}.yaml", "rolebinding/#{env[:name]}.yaml", "#{env[:kind]}/#{env[:name]}.yaml"])
       expect(result[:patches]).to be_a(Array)
       expect(result[:patches]).to eq([{path: "configmap/#{env[:name]}.yaml"}])
     end
 
     it 'returns the correct kustomization (add)' do
-      result = _base.send(:_kustomization, kustomization, true)
+      result = _base.send(:_kustomization, kustomization, true, true)
       expect(result).to be_a(Hash)
       expect(result[:configMapGenerator]).to be_a(Array)
       expect(result[:configMapGenerator]).to eq([{ name: 'rspec-config', options: { disableNameSuffixHash: true } }, {name: env[:name], options: {disableNameSuffixHash: true}}])
       expect(result[:resources]).to be_a(Array)
-      expect(result[:resources]).to eq(["#{env[:kind]}/rspec-workflow.yaml", "serviceaccount/#{env[:service_account]}.yaml", "rolebinding/#{env[:name]}-#{env[:service_account]}.yaml", "#{env[:kind]}/#{env[:name]}.yaml"])
+      expect(result[:resources]).to eq(["#{env[:kind]}/rspec-workflow.yaml", "serviceaccount/#{env[:name]}.yaml", "rolebinding/#{env[:name]}.yaml", "#{env[:kind]}/#{env[:name]}.yaml"])
       expect(result[:patches]).to be_a(Array)
       expect(result[:patches]).to eq([{ path: "configmap/rspec-config.yaml" }, { path: "configmap/#{env[:name]}.yaml" }])
     end
@@ -62,23 +62,23 @@ describe 'Base' do
           { path: "configmap/rspec-config.yaml" },
         ],
       }
-      result = _base.send(:_kustomization, values, true)
+      result = _base.send(:_kustomization, values, true, true)
       expect(result).to be_a(Hash)
       expect(result[:configMapGenerator]).to be_a(Array)
       expect(result[:configMapGenerator]).to eq([{ name: 'rspec-config', options: { disableNameSuffixHash: true } }, {name: env[:name], options: {disableNameSuffixHash: true}}])
       expect(result[:resources]).to be_a(Array)
-      expect(result[:resources]).to eq(["#{env[:kind]}/rspec-workflow.yaml", "serviceaccount/#{env[:service_account]}.yaml", "rolebinding/#{env[:name]}-#{env[:service_account]}.yaml", "#{env[:kind]}/#{env[:name]}.yaml"])
+      expect(result[:resources]).to eq(["#{env[:kind]}/rspec-workflow.yaml", "serviceaccount/#{env[:name]}.yaml", "rolebinding/#{env[:name]}.yaml", "#{env[:kind]}/#{env[:name]}.yaml"])
       expect(result[:patches]).to be_a(Array)
       expect(result[:patches]).to eq([{ path: "configmap/rspec-config.yaml" }, { path: "configmap/#{env[:name]}.yaml" }])
     end
 
     it 'returns the correct kustomization (do not create blank patches)' do
-      result = _base.send(:_kustomization, kustomization, false)
+      result = _base.send(:_kustomization, kustomization, false, false)
       expect(result).to be_a(Hash)
       expect(result[:configMapGenerator]).to be_a(Array)
       expect(result[:configMapGenerator]).to eq([{ name: 'rspec-config', options: { disableNameSuffixHash: true } }, {name: env[:name], options: {disableNameSuffixHash: true}}])
       expect(result[:resources]).to be_a(Array)
-      expect(result[:resources]).to eq(["#{env[:kind]}/rspec-workflow.yaml", "serviceaccount/#{env[:service_account]}.yaml", "rolebinding/#{env[:name]}-#{env[:service_account]}.yaml", "#{env[:kind]}/#{env[:name]}.yaml"])
+      expect(result[:resources]).to eq(["#{env[:kind]}/rspec-workflow.yaml", "#{env[:kind]}/#{env[:name]}.yaml"])
       expect(result[:patches]).to be_a(Array)
       expect(result[:patches]).to eq([{ path: "configmap/rspec-config.yaml" }])
     end
@@ -103,31 +103,41 @@ describe 'Base' do
 
   describe '#_serviceAccount' do
     it 'returns the correct ServiceAccount' do
-      result = _base.send(:_serviceAccount)
+      result = _base.send(:_serviceAccount, true)
       expect(result).to be_a(Hash)
       expect(result[:kind]).to eq('ServiceAccount')
+    end
+
+    it 'returns the correct ServiceAccount (do not create)' do
+      result = _base.send(:_serviceAccount, false)
+      expect(result).to be(nil)
     end
   end
 
   describe '#_roleBinding' do
     it 'returns the correct RoleBinding' do
-      result = _base.send(:_roleBinding)
+      result = _base.send(:_roleBinding, true)
       expect(result).to be_a(Hash)
       expect(result[:kind]).to eq('RoleBinding')
+    end
+
+    it 'returns the correct RoleBinding (do not create)' do
+      result = _base.send(:_roleBinding, false)
+      expect(result).to be(nil)
     end
   end
 
   describe '#_workflow' do
     it 'returns the correct CronWorkflow' do
-      base = BaseManifest.new(env[:workspace], env[:service], env[:owner], env[:namespace], 'CronWorkflow', env[:name], env[:service_account])
-      result = base.send(:_workflow)
+      base = BaseManifest.new(env[:workspace], env[:service], env[:owner], env[:namespace], 'CronWorkflow', env[:name])
+      result = base.send(:_workflow, true)
       expect(result).to be_a(Hash)
       expect(result[:kind]).to eq('CronWorkflow')
     end
 
     it 'returns the correct WorkflowTemplate' do
-      base = BaseManifest.new(env[:workspace], env[:service], env[:owner], env[:namespace], 'WorkflowTemplate', env[:name], env[:service_account])
-      result = base.send(:_workflow)
+      base = BaseManifest.new(env[:workspace], env[:service], env[:owner], env[:namespace], 'WorkflowTemplate', env[:name])
+      result = base.send(:_workflow, false)
       expect(result).to be_a(Hash)
       expect(result[:kind]).to eq('WorkflowTemplate')
     end
