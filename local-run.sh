def60fa34c72a0ad570502eab242caad4969c1ba
panicboat <panicboat@gmail.com>
2026-03-01 08:30:14 +0900

    Create local-run.sh

---
+++ local-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pkill -f gruf
+pkill -f next
+
+cd services/monolith/workspace
+bundle install && bin/codegen && bin/grpc &
+
+cd web/nyx/workspace
+pnpm install && pnpm proto:gen && pnpm dev &
+
+wait
