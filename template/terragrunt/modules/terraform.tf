c6b0c779986e431c73afbc620e720bf3c5fa6691
panicboat <panicboat@gmail.com>
2026-05-02 16:50:26 +0900

    chore: Specify exact Terraform and provider versions (#582)

--- template/terragrunt/modules/terraform.tf
+++ template/terragrunt/modules/terraform.tf
@@ -1,16 +1,16 @@
 # terraform.tf - Terraform configuration for GitHub OIDC Auth
 
 terraform {
-  required_version = ">= 1.15.0"
+  required_version = "1.15.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.43"
+      version = "6.43.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.2"
+      version = "4.2.0"
     }
   }
 }
