2123922af7d9b2474ad414b806f1a79bbcc64032
panicboat <panicboat@gmail.com>
2026-05-17 16:33:32 +0900

    chore: Update Terraform version requirements (#629)

--- services/monolith/terragrunt/modules/terraform.tf
+++ services/monolith/terragrunt/modules/terraform.tf
@@ -1,12 +1,12 @@
 # terraform.tf - Terraform configuration for monolith RDS module
 
 terraform {
-  required_version = ">= 1.11.0"
+  required_version = "1.11.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.43.0"
+      version = "6.44.0"
     }
     random = {
       source  = "hashicorp/random"
