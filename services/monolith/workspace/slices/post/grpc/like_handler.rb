f9684ceb4748f188812c45570faedc94a0515f71
panicboat <panicboat@gmail.com>
2026-06-07 18:09:11 +0900

    feat(post): implement symmetric LikeService (additive)
    Signed-off-by: panicboat <panicboat@gmail.com>

--- services/monolith/workspace/slices/post/grpc/like_handler.rb
+++ services/monolith/workspace/slices/post/grpc/like_handler.rb
@@ -17,6 +17,9 @@ module Post
       rpc :LikeCastPost, ::Post::V1::LikeCastPostRequest, ::Post::V1::LikeCastPostResponse
       rpc :UnlikeCastPost, ::Post::V1::UnlikeCastPostRequest, ::Post::V1::UnlikeCastPostResponse
       rpc :GetPostLikeStatus, ::Post::V1::GetPostLikeStatusRequest, ::Post::V1::GetPostLikeStatusResponse
+      rpc :LikePost, ::Post::V1::LikePostRequest, ::Post::V1::LikePostResponse
+      rpc :UnlikePost, ::Post::V1::UnlikePostRequest, ::Post::V1::UnlikePostResponse
+      rpc :GetLikeStatus, ::Post::V1::GetLikeStatusRequest, ::Post::V1::GetLikeStatusResponse
 
       include Post::Deps[
         like_post_uc: "use_cases.likes.like_post",
@@ -63,6 +66,37 @@ module Post
         end
 
         ::Post::V1::GetPostLikeStatusResponse.new(liked: liked)
+      end
+
+      # ---- Symmetric (account-based) API ----
+
+      def like_post
+        authenticate_user!
+
+        post = post_repo.find_by_id(request.message.post_id)
+        raise GRPC::BadStatus.new(GRPC::Core::StatusCodes::NOT_FOUND, "Post not found") unless post
+
+        like_repo.account_like(post_id: request.message.post_id, account_id: current_user_id)
+        ::Post::V1::LikePostResponse.new(likes_count: like_repo.likes_count(post_id: request.message.post_id))
+      end
+
+      def unlike_post
+        authenticate_user!
+
+        like_repo.account_unlike(post_id: request.message.post_id, account_id: current_user_id)
+        ::Post::V1::UnlikePostResponse.new(likes_count: like_repo.likes_count(post_id: request.message.post_id))
+      end
+
+      def get_like_status
+        post_ids = request.message.post_ids.to_a
+
+        liked = if current_user_id
+          like_repo.account_liked_status_batch(post_ids: post_ids, account_id: current_user_id)
+        else
+          post_ids.each_with_object({}) { |id, h| h[id] = false }
+        end
+
+        ::Post::V1::GetLikeStatusResponse.new(liked: liked)
       end
 
       private
