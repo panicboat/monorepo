187abbb8db32ad2f54da5d01b8d10cf003cf1a25
panicboat <panicboat@gmail.com>
2026-06-18 02:19:27 +0900

    feat(footprints): scaffold slice with visit + read_state relations
    Signed-off-by: panicboat <panicboat@gmail.com>

---
+++ services/monolith/workspace/slices/footprints/relations/visit_records.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Footprints
+  module Relations
+    class VisitRecords < Footprints::DB::Relation
+      schema(:"footprints__visits", as: :visit_records, infer: false) do
+        attribute :id, Types::String
+        attribute :visitor_id, Types::String
+        attribute :visited_id, Types::String
+        attribute :first_visited_at, Types::Time
+        attribute :last_visited_at, Types::Time
+        attribute :created_at, Types::Time
+        attribute :updated_at, Types::Time
+
+        primary_key :id
+      end
+    end
+  end
+end
