99d663d3d7cb591e8a8fe59199814b2684a7c089
panicboat <panicboat@gmail.com>
2026-06-17 15:42:15 +0900

    feat(messaging): StartChatButton on /u/[username] (GetOrCreateThread + navigate)
    Signed-off-by: panicboat <panicboat@gmail.com>

--- services/frontend/workspace/src/modules/messaging/index.ts
+++ services/frontend/workspace/src/modules/messaging/index.ts
@@ -1,3 +1,4 @@
 export * from "./types";
 export * from "./hooks";
 export { MessagingStreamProvider } from "./providers/MessagingStreamProvider";
+export { StartChatButton } from "./components/StartChatButton";
