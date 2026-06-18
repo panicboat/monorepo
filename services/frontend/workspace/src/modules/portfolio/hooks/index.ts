1bc3c7f20d8b517a61b473babdc718be78547628
panicboat <panicboat@gmail.com>
2026-06-18 10:17:05 +0900

    chore(frontend): remove dead portfolio module + 22 unused BFFs + portfolio/offer grpc clients (#732)
    Signed-off-by: panicboat <panicboat@gmail.com>

--- services/frontend/workspace/src/modules/portfolio/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-export { useCastProfile } from "./useCastProfile";
-export { useCastPlans } from "./useCastPlans";
-export { useCastSchedules } from "./useCastSchedules";
-export { useCastImages } from "./useCastImages";
-export { useCastData } from "./useCastData";
-export { useAreas } from "./useAreas";
-export { useGenres } from "./useGenres";
-export { useInfiniteCasts } from "./useInfiniteCasts";
