ec6c8b09431925ff2a6880f88ed9980d071569cb
panicboat <panicboat@gmail.com>
2026-08-28 08:40:30 +0900

    feat(monolith/billing): add stripe gem and billing settings scaffolding
    Signed-off-by: panicboat <panicboat@gmail.com>

--- dystopia/monolith/config/settings.rb
+++ dystopia/monolith/config/settings.rb
@@ -2,8 +2,12 @@
 
 module Monolith
   class Settings < Hanami::Settings
-    # Define your app settings here, for example:
-    #
-    # setting :my_flag, default: false, constructor: Types::Params::Bool
+    setting :stripe_api_key, constructor: Types::String
+    setting :stripe_webhook_secret, constructor: Types::String
+    setting :stripe_price_id_guest, constructor: Types::String
+    setting :stripe_price_id_cast, constructor: Types::String
+    setting :billing_success_url, constructor: Types::String
+    setting :billing_cancel_url, constructor: Types::String
+    setting :billing_portal_return_url, constructor: Types::String
   end
 end
