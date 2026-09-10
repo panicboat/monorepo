061f8bff9e2b43e54c5f84cd2f03f5f9d192c48c
panicboat <panicboat@gmail.com>
2026-09-11 00:34:29 +0900

    feat(meeting-translation): deploy translation service
    Signed-off-by: panicboat <panicboat@gmail.com>

--- dystopia/infrastructure/aws/modules/pod_identity.tf
+++ dystopia/infrastructure/aws/modules/pod_identity.tf
@@ -43,3 +43,56 @@ resource "aws_eks_pod_identity_association" "monolith" {
 
   tags = var.common_tags
 }
+
+resource "aws_iam_role" "meeting_translation" {
+  name = "meeting-translation-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "pods.eks.amazonaws.com"
+      }
+      Action = ["sts:AssumeRole", "sts:TagSession"]
+    }]
+  })
+
+  tags = var.common_tags
+}
+
+resource "aws_iam_policy" "meeting_translation" {
+  name = "meeting-translation-${var.environment}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["transcribe:StartStreamTranscription"]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["bedrock:InvokeModel"]
+        Resource = "arn:aws:bedrock:${var.aws_region}::foundation-model/amazon.nova-lite-v1:0"
+      },
+    ]
+  })
+
+  tags = var.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "meeting_translation" {
+  role       = aws_iam_role.meeting_translation.name
+  policy_arn = aws_iam_policy.meeting_translation.arn
+}
+
+resource "aws_eks_pod_identity_association" "meeting_translation" {
+  cluster_name    = data.aws_eks_cluster.this.name
+  namespace       = "dystopia"
+  service_account = "meeting-translation"
+  role_arn        = aws_iam_role.meeting_translation.arn
+
+  tags = var.common_tags
+}
