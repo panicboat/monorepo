402a4f4edcfc65f097abc3b671991c482fd76b68
panicboat <panicboat@gmail.com>
2026-09-21 16:40:47 +0900

    refactor(dystopia/monolith): delete dead cast/guest-adapter comment author code
    Signed-off-by: panicboat <panicboat@gmail.com>

--- dystopia/monolith/slices/post/grpc/handler.rb
+++ dystopia/monolith/slices/post/grpc/handler.rb
@@ -54,26 +54,10 @@ module Post
         cast_adapter.find_by_user_id(current_user_id)
       end
 
-      def find_my_cast!
-        cast = find_my_cast
-        unless cast
-          raise GRPC::BadStatus.new(GRPC::Core::StatusCodes::NOT_FOUND, "Cast profile not found")
-        end
-        cast
-      end
-
       def find_my_guest
         return nil unless current_user_id
 
         guest_adapter.find_by_user_id(current_user_id)
-      end
-
-      def find_my_guest!
-        guest = find_my_guest
-        unless guest
-          raise GRPC::BadStatus.new(GRPC::Core::StatusCodes::NOT_FOUND, "Guest profile not found")
-        end
-        guest
       end
 
       def find_blocker
@@ -88,59 +72,11 @@ module Post
         nil
       end
 
-      def find_blocker!
-        blocker = find_blocker
-        unless blocker
-          raise GRPC::BadStatus.new(GRPC::Core::StatusCodes::NOT_FOUND, "User profile not found")
-        end
-        blocker
-      end
-
       def get_blocked_user_ids
         blocker = find_blocker
         return [] unless blocker
 
         block_adapter.blocked_ids(account_id: blocker[:id])
-      end
-
-      def get_comment_author(user_id, media_files: {})
-        user_type = account_adapter.get_user_type(user_id)
-        return nil unless user_type
-
-        if user_type == "cast"
-          cast = cast_adapter.find_by_user_id(user_id)
-          if cast
-            media_id = cast.avatar_media_id.to_s.empty? ? cast.profile_media_id : cast.avatar_media_id
-            media_file = media_files[media_id]
-            {
-              id: cast.user_id,
-              name: cast.name,
-              image_url: media_file&.url || "",
-              user_type: "cast"
-            }
-          else
-            { id: user_id, name: "Anonymous Cast", image_url: "", user_type: "cast" }
-          end
-        else
-          guest = guest_adapter.find_by_user_id(user_id)
-          if guest
-            media_file = media_files[guest.avatar_media_id]
-            {
-              id: guest.user_id,
-              name: guest.name,
-              image_url: media_file&.url || "",
-              user_type: "guest"
-            }
-          else
-            { id: user_id, name: "Guest", image_url: "", user_type: "guest" }
-          end
-        end
-      end
-
-      def load_authors(cast_ids)
-        return {} if cast_ids.empty?
-
-        cast_adapter.find_by_cast_ids(cast_ids)
       end
     end
   end
